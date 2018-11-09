5841c82f066102710c6f67cdadfd0bdaa946ba9a
fifiten82726 <fifiteen82726@gmail.com>
2018-11-08 19:49:29 -0500

    Add: basic notification

--- app/channels/web_notifications_channel.rb
+++ app/channels/web_notifications_channel.rb
@@ -1,10 +1,8 @@
 class WebNotificationsChannel < ApplicationCable::Channel
   def subscribed
     stream_from "web_notifications_channel"
-    stream_from "web_notifications_channel#{message_user.id}"
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
   end
 end
