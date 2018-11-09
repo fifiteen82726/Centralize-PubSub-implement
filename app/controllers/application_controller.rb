5328ec6aa5d9531749b79550870c95ee25c671a2
fifiten82726 <fifiteen82726@gmail.com>
2018-11-08 22:12:56 -0500

    Add: ok method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def ok(info={})
+    render json: { status: 'ok', data: info }
+    true
+  end
 end
